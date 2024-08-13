ede6ab2ca1148a70e999525c7467ee71dc402afc
Kennix88 <shel.live@yandex.ru>
2024-08-13 20:32:26 +0800

    Заполнение демо данных

---
+++ src/data/data.ts
@@ -0,0 +1,515 @@
+export const dataProject = {
+    products: {
+        coffee: [
+            {
+                key: 'espresso',
+                name: 'Экспрессо',
+                rate: 0,
+                image: 'espresso.png',
+                isSyrup: false,
+                price: {
+                    small: {
+                        volume: 25,
+                        amount: 79
+                    },
+                    medium: {
+                        volume: 50,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 75,
+                        amount: 237
+                    }
+                }
+            },
+            {
+                key: 'double-espresso',
+                name: 'Экспрессо',
+                rate: 2,
+                image: 'espresso.png',
+                price: {
+                    small: {
+                        volume: 50,
+                        amount: 109
+                    },
+                    medium: {
+                        volume: 100,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 150,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'amerikano',
+                name: 'Американо',
+                rate: 0,
+                image: 'amerikano.png',
+                price: {
+                    small: {
+                        volume: 120,
+                        amount: 119
+                    },
+                    medium: {
+                        volume: 155,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 180,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'latte',
+                name: 'Латте',
+                rate: 0,
+                image: 'latte.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'cappuccino',
+                name: 'Капучино',
+                rate: 0,
+                image: 'appuccino.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'makiato',
+                name: 'Макиато',
+                rate: 0,
+                image: 'makiato.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            }
+        ],
+        tea: [
+            {
+                key: 'tea1',
+                name: 'Чай',
+                rate: 0,
+                image: 'tea.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'tea2',
+                name: 'Чай',
+                rate: 0,
+                image: 'tea.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'tea3',
+                name: 'Чай',
+                rate: 0,
+                image: 'tea.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'tea4',
+                name: 'Чай',
+                rate: 0,
+                image: 'tea.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'tea5',
+                name: 'Чай',
+                rate: 0,
+                image: 'tea.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'tea6',
+                name: 'Чай',
+                rate: 0,
+                image: 'tea.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            }
+        ],
+        milk: [
+            {
+                key: 'milk1',
+                name: 'Коктель',
+                rate: 0,
+                image: 'milk.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'milk2',
+                name: 'Коктель',
+                rate: 0,
+                image: 'milk.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'milk3',
+                name: 'Коктель',
+                rate: 0,
+                image: 'milk.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'milk4',
+                name: 'Коктель',
+                rate: 0,
+                image: 'milk.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'milk5',
+                name: 'Коктель',
+                rate: 0,
+                image: 'milk.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'milk6',
+                name: 'Коктель',
+                rate: 0,
+                image: 'milk.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            }
+        ],
+        drinks: [
+            {
+                key: 'drink1',
+                name: 'Кола',
+                rate: 0,
+                image: 'drink.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'drink2',
+                name: 'Кола',
+                rate: 0,
+                image: 'drink.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'drink3',
+                name: 'Кола',
+                rate: 0,
+                image: 'drink.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'drink4',
+                name: 'Кола',
+                rate: 0,
+                image: 'drink.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'drink5',
+                name: 'Кола',
+                rate: 0,
+                image: 'drink.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            },
+            {
+                key: 'drink6',
+                name: 'Кола',
+                rate: 0,
+                image: 'drink.png',
+                price: {
+                    small: {
+                        volume: 200,
+                        amount: 129
+                    },
+                    medium: {
+                        volume: 300,
+                        amount: 158
+                    },
+                    big: {
+                        volume: 400,
+                        amount: 327
+                    }
+                }
+            }
+        ],
+        syrup: [
+            {
+                key: 'vanilla',
+                name: 'Ванильный сироп',
+                price: 1
+            },
+            {
+                key: 'mint',
+                name: 'Мятный сироп',
+                price: 2
+            },
+            {
+                key: 'caramel',
+                name: 'Кармельный сироп',
+                price: 2
+            },
+            {
+                key: 'chocolate',
+                name: 'Шоколадный сироп',
+                price: 3
+            }
+        ]
+    },
+}
